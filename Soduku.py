0c9ab80ec33ed097efd2160ad73bc50f7e53f2e3
JonathanQi915 <qi.jo@northeastern.edu>
2022-11-18 03:13:35 -0500

    Skeleton

---
+++ Soduku.py
@@ -0,0 +1,27 @@
+class Sudoku:
+  def __init__(self):
+    self.board = []
+
+  def make_empty_board(self):
+    pass
+
+  def placer(self, row, col, val):
+    pass
+
+  def print_board(self):
+    pass
+
+  def is_board_correct(self):
+    pass
+
+  def generate_board(self):
+    pass
+
+  def solutions_count(self):
+    pass
+
+  def solve(self):
+    pass
+
+  def start(self):
+    pass
