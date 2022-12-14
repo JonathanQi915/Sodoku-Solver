1317ee3dbfff59c0495bf7a65e1272ad13bab9d4
Jonathan <qi.jo@northeastern.edu>
2022-12-13 23:22:32 -0500

    Generator + playing through terminal

--- Soduku.py
+++ Soduku.py
@@ -1,36 +1,18 @@
-import numpy as np;
+import numpy as np
+import copy
+from random import shuffle
+from random import sample
 class Sudoku:
-  def __init__(self, num):
-    if num == 1:
-      self.board = [[8,9,0,0,6,2,0,0,0],
-                    [0,0,0,0,8,0,0,4,0],
-                    [0,1,7,0,0,5,2,0,0],
-                    [7,0,0,0,0,1,0,0,0],
-                    [0,6,8,0,0,0,0,5,4],
-                    [0,0,0,0,5,0,8,0,7],
-                    [6,3,1,5,2,0,9,0,8],
-                    [5,8,0,7,3,0,4,1,2],
-                    [0,0,0,9,1,0,0,6,0]]
-    elif num == 2:
-      self.board = [[0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [1,0,0,0,0,0,0,0,0]]
-    elif num == 3: 
-      self.board = [[8,9,0,0,6,2,0,0,0],
-                    [0,0,0,0,8,0,0,4,0],
-                    [0,1,7,0,0,5,2,0,0],
-                    [7,0,0,0,0,1,0,0,0],
-                    [0,6,8,0,0,0,0,5,4],
-                    [0,0,0,0,5,0,8,0,7],
-                    [6,3,1,5,2,0,0,0,8],
-                    [5,8,0,7,3,0,4,1,2],
-                    [0,0,0,9,1,0,0,6,0]]
+  def __init__(self, arr = [[0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,0]]): #default to empty unless given an array
+    self.board = arr
   def make_empty_board(self):
     self.board = [[0,0,0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0,0,0],
@@ -40,14 +22,20 @@ class Sudoku:
                   [0,0,0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0,0,0],
-                  [1,0,0,0,0,0,0,0,0]]
+                  [0,0,0,0,0,0,0,0,0]]
 
   def place(self, x, y, n):
     self.board[y][x] = n
 
+  def remove(self, x, y):
+    self.board[y][x] = 0
+
   # easy print method, will improve later
   def print_board(self):
     print(np.matrix(self.board))
+
+  def is_empty(self, x, y):
+    return self.board[y][x] == 0
 
   def is_valid_move(self, x, y, n) : 
     x = x - 1
@@ -65,23 +53,60 @@ class Sudoku:
           return False 
     return True
 
-  def is_board_correct(self):
+  def is_board_solved(self):
     for y in range(9):
       for x in range(9):
-       if self.board[y][x] != 0:     
-        if not (self.is_valid_move(x + 1, y + 1, self.board[y][x])):
-          return False 
+       if self.board[y][x] == 0:     
+        return False 
     return True
-
+  
+  def numbers_count(self):
+    count = 0
+    for y in range(9):
+      for x in range(9): # checks every square on the board
+        if not self.board[y][x] == 0: # for every square that is not 0 increase the count
+          count += 1
+    return count
 
   def generate_board(self):
-    pass
+    self.generate_solved_board()
+    not_empty = []
+    for i in range(9):
+      for j in range(9):
+        if self.board[i][j] != 0:
+          not_empty.append((i, j)) # stores all not empty squares as tuples
+    shuffle(not_empty)
+    not_empty_count = len(not_empty)
+    rounds = 3
+    while rounds > 0 and not_empty_count >= 17:
+      x, y = not_empty.pop()
+      not_empty_count -= 1
+      removed_square = self.board[y][x]
+      self.remove(x,y)
+      if self.solutions_count() != 1:
+        self.board[y][x] = removed_square
+        not_empty_count += 1
+        rounds -= 1
+    return
+  
+  def generate_solved_board(self):
+    # pattern for what the solution should be 
+    def pattern(r,c): return (3 * (r % 3) + r // 3 + c) % 9
+    # randomize everything, but make sure its the same pattern
+    def shuffle(s): return sample(s,len(s)) 
+    rows  = [ g * 3 + r for g in shuffle(range(3)) for r in shuffle(range(3)) ] 
+    cols  = [ g * 3 + c for g in shuffle(range(3)) for c in shuffle(range(3)) ]
+    nums  = shuffle(range(1, 3 * 3 + 1))
+    self.board = [[nums[pattern(r,c)] for c in cols] for r in rows]
+
   
   count = 0
   def solutions_count(self):
+    board_copy = copy.deepcopy(self.board) # copy so that it doesn't mess up the actual board
     self.solutions_count_helper()
     countCopy = self.count
     self.count = 0
+    self.board = copy.deepcopy(board_copy)
     return countCopy
   
   def solutions_count_helper(self):
@@ -112,20 +137,27 @@ class Sudoku:
     print()
 
   def start(self):
-    pass
+    self.generate_board()
+    print("Your sudoku board is: ")
+    print()
+    self.print_board()
+    while not self.is_board_solved():
+      n = int(input("Enter the number you wish to place: "))
+      x = int(input("Enter the column you wish to place the number in: "))
+      y = int(input("Enter the row you wish to place the number in: "))
+      if self.is_valid_move(x, y, n) and self.is_empty(x - 1, y - 1):
+        self.place(x - 1, y - 1, n)
+        self.print_board()
+      else:
+        print("Number unable to be placed in that spot")
+        flag = input("Would you like the solution? 'Y' if yes, 'N' if no: ")
+        if flag == "Y":
+          print("The solution is: ")
+          self.solve()
+          return
+    print("Congratulations! You won!")
+    return
 
-# print debugging
-MySudoku = Sudoku(3)
-MySudoku.print_board()
-MySudoku.solve()
-print(MySudoku.solutions_count())
-print(MySudoku.count)
-MySudoku.board[0][0]
-print(MySudoku.is_board_correct())
-print(MySudoku.is_valid_move(1,1,1))
-print(MySudoku.is_valid_move(2,2,2))
-print(MySudoku.is_valid_move(4,2,5))
-print(MySudoku.is_valid_move(2,4,5))
-print(MySudoku.is_valid_move(2,2,4))
-print(MySudoku.is_valid_move(2,2,7))
-print(MySudoku.is_valid_move(2,2,3))
+MySudoku = Sudoku()
+MySudoku.start()
+    
